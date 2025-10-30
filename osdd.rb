0a1a87bb7e33156bdd42dd49d208a36e466d79b4
GitHub Action <action@github.com>
2025-10-30 01:20:53 +0000

    Update osdd formula to version 0.2.0
    Updated formula with:
      - Version: 0.2.0
      - AMD64 SHA256: 5fb5a0cc7d5bc63775d5125f62a605d59f5df9bcd058d39c3ec59e65894d3254
      - ARM64 SHA256: 84d12ed5bb9ea9982373cae0b462f0259bee1e7434be18aec651ce020ff25e52

--- osdd.rb
+++ osdd.rb
@@ -6,12 +6,12 @@ class Osdd < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/opensdd/osdd-cli/releases/download/v#{version}/osdd-darwin-arm64-noautoupdate.tar.gz"
+      url "https://github.com/opensdd/osdd-cli/releases/download/v#{version}/osdd-darwin-arm64.tar.gz"
       sha256 "84d12ed5bb9ea9982373cae0b462f0259bee1e7434be18aec651ce020ff25e52"
     end
 
     on_intel do
-      url "https://github.com/opensdd/osdd-cli/releases/download/v#{version}/osdd-darwin-amd64-noautoupdate.tar.gz"
+      url "https://github.com/opensdd/osdd-cli/releases/download/v#{version}/osdd-darwin-amd64.tar.gz"
       sha256 "5fb5a0cc7d5bc63775d5125f62a605d59f5df9bcd058d39c3ec59e65894d3254"
     end
   end
