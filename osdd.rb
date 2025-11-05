9b41b73136c0f4e7785ca1e4455fba4df6969d00
GitHub Action <action@github.com>
2025-11-05 18:48:59 +0000

    Update osdd formula to version 0.4.0
    Updated formula with:
      - Version: 0.4.0
      - AMD64 SHA256: 17600c994bf66c729740e0b05d15853908fbeb53c9ed4abae0e7239465173d74
      - ARM64 SHA256: 6dc5aa82e94159be78019debab63f1705b49714032b2db8c0a1bc0ec33513680

--- osdd.rb
+++ osdd.rb
@@ -1,18 +1,18 @@
 class Osdd < Formula
   desc "OpenSDD CLI tool"
   homepage "https://github.com/opensdd/osdd-cli"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/opensdd/osdd-cli/releases/download/v#{version}/osdd-darwin-arm64.tar.gz"
-      sha256 "ee13774b88fa9abb3823530929ea5af2b4ac782910db8df51bc2c35f7a7c7f3a"
+      sha256 "6dc5aa82e94159be78019debab63f1705b49714032b2db8c0a1bc0ec33513680"
     end
 
     on_intel do
       url "https://github.com/opensdd/osdd-cli/releases/download/v#{version}/osdd-darwin-amd64.tar.gz"
-      sha256 "eabb011717411f969d23e92bb690a774df3ee93cdcd9a23952b5cfebbee8f0c6"
+      sha256 "17600c994bf66c729740e0b05d15853908fbeb53c9ed4abae0e7239465173d74"
     end
   end
 
